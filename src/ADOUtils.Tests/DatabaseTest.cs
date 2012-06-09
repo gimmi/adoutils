8cbd82bfe4d84e9f2d662a339e9e4e9fc0b7f7f5
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-06-09 18:45:13 +0200

    Added test

--- src/ADOUtils.Tests/DatabaseTest.cs
+++ src/ADOUtils.Tests/DatabaseTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using NUnit.Framework;
@@ -10,9 +11,9 @@ namespace ADOUtils.Tests
 	public class DatabaseTest
 	{
 		public const string Script = @"
-CREATE TABLE Tbl(Id int NULL, Name nvarchar(255) NULL)
-INSERT Tbl(Id, Name) VALUES(1, 'row 1')
-INSERT Tbl(Id, Name) VALUES(2, 'row 2')
+CREATE TABLE Tbl(IntValue int NULL, StringValue nvarchar(255) NULL, DateValue datetime NULL, GuidValue uniqueidentifier NULL)
+INSERT Tbl(IntValue, StringValue, DateValue, GuidValue) VALUES(1, 'string 1', '2012-06-09 18:33', 'A71C8E84-B1A1-4CED-81B7-F551704A33E7')
+INSERT Tbl(IntValue, StringValue, DateValue, GuidValue) VALUES(2, 'string 2', '2012-06-09 18:34', 'A9610CE3-7013-4C78-9C32-452D5A3CE450')
 ";
 		private Database _target;
 
@@ -28,32 +29,47 @@ INSERT Tbl(Id, Name) VALUES(2, 'row 2')
 		[Test]
 		public void Should_query_with_yield()
 		{
-			_target.Yield("SELECT Name FROM Tbl WHERE Id = @Id", new Dictionary<string, object> { { "Id", 2 } }).Select(x => x.GetString(0)).First().Should().Be.EqualTo("row 2");
-			_target.Yield("SELECT Name FROM Tbl WHERE Id = @Id", new { Id = 2 }).Select(x => x.GetString(0)).First().Should().Be.EqualTo("row 2");
-			_target.Yield("SELECT Name FROM Tbl WHERE Id = 2").Select(x => x.GetString(0)).First().Should().Be.EqualTo("row 2");
+			_target.Yield("SELECT StringValue FROM Tbl WHERE IntValue = @IntValue", new Dictionary<string, object> { { "IntValue", 2 } }).Select(x => x.GetString(0)).First().Should().Be.EqualTo("string 2");
+			_target.Yield("SELECT StringValue FROM Tbl WHERE IntValue = @IntValue", new { IntValue = 2 }).Select(x => x.GetString(0)).First().Should().Be.EqualTo("string 2");
+			_target.Yield("SELECT StringValue FROM Tbl WHERE IntValue = 2").Select(x => x.GetString(0)).First().Should().Be.EqualTo("string 2");
 		}
 
 		[Test]
 		public void Should_query_with_read()
 		{
-			_target.Read("SELECT Name FROM Tbl WHERE Id = @Id", new Dictionary<string, object> { { "Id", 2 } }).Select(x => x["Name"]).First().Should().Be.EqualTo("row 2");
-			_target.Read("SELECT Name FROM Tbl WHERE Id = @Id", new { Id = 2 }).Select(x => x["Name"]).First().Should().Be.EqualTo("row 2");
-			_target.Read("SELECT Name FROM Tbl WHERE Id = 2").Select(x => x["Name"]).First().Should().Be.EqualTo("row 2");
+			_target.Read("SELECT StringValue FROM Tbl WHERE IntValue = @IntValue", new Dictionary<string, object> { { "IntValue", 2 } }).Select(x => x["StringValue"]).First().Should().Be.EqualTo("string 2");
+			_target.Read("SELECT StringValue FROM Tbl WHERE IntValue = @IntValue", new { IntValue = 2 }).Select(x => x["StringValue"]).First().Should().Be.EqualTo("string 2");
+			_target.Read("SELECT StringValue FROM Tbl WHERE IntValue = 2").Select(x => x["StringValue"]).First().Should().Be.EqualTo("string 2");
 		}
 
 		[Test]
 		public void Should_query_scalar()
 		{
 			_target.Scalar<int>("SELECT COUNT(*) FROM Tbl").Should().Be.EqualTo(2);
-			_target.Scalar<int>("SELECT COUNT(*) FROM Tbl WHERE Id = @Id", new Dictionary<string, object> { { "Id", 2 } }).Should().Be.EqualTo(1);
-			_target.Scalar<int>("SELECT COUNT(*) FROM Tbl WHERE Id = @Id", new { Id = 2 }).Should().Be.EqualTo(1);
+			_target.Scalar<int>("SELECT COUNT(*) FROM Tbl WHERE IntValue = @IntValue", new Dictionary<string, object> { { "IntValue", 2 } }).Should().Be.EqualTo(1);
+			_target.Scalar<int>("SELECT COUNT(*) FROM Tbl WHERE IntValue = @IntValue", new { IntValue = 2 }).Should().Be.EqualTo(1);
 		}
 
 		[Test]
 		public void Should_exec()
 		{
-			_target.Exec("UPDATE Tbl SET Name = Name").Should().Be.EqualTo(2);
-			_target.Exec("UPDATE Tbl SET Name = Name WHERE 1 = @par", new { par = 2 }).Should().Be.EqualTo(0);
+			_target.Exec("UPDATE Tbl SET StringValue = StringValue").Should().Be.EqualTo(2);
+			_target.Exec("UPDATE Tbl SET StringValue = StringValue WHERE 1 = @par", new { par = 2 }).Should().Be.EqualTo(0);
+		}
+
+		[Test]
+		public void Should_pass_and_read_various_datatypes_as_expected()
+		{
+			var actual = _target.Yield("SELECT * FROM Tbl WHERE IntValue = @IntValue AND StringValue = @StringValue AND DateValue = @DateValue AND GuidValue = @GuidValue", new {
+				IntValue = 1, StringValue = "string 1", DateValue = new DateTime(2012, 6, 9, 18, 33, 0), GuidValue = new Guid("A71C8E84-B1A1-4CED-81B7-F551704A33E7")
+			}).Select(r => new {
+				IntValue = r["IntValue"], StringValue = r["StringValue"], DateValue = r["DateValue"], GuidValue = r["GuidValue"]
+			}).Single();
+
+			actual.IntValue.Should().Be.EqualTo(1);
+			actual.StringValue.Should().Be.EqualTo("string 1");
+			actual.DateValue.Should().Be.EqualTo(new DateTime(2012, 6, 9, 18, 33, 0));
+			actual.GuidValue.Should().Be.EqualTo(new Guid("A71C8E84-B1A1-4CED-81B7-F551704A33E7"));
 		}
 
 		[Test]
