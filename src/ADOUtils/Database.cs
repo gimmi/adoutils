779f5215fd53bc6244624c0aa667c91711cdb3cf
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-06-23 08:05:09 +0200

    Reordered constructor parameters

--- src/ADOUtils/Database.cs
+++ src/ADOUtils/Database.cs
@@ -14,9 +14,9 @@ namespace ADOUtils
 		private IDbConnection _conn;
 		private IDbTransaction _tr;
 
-		public Database(string connStr) : this(DbProviderFactories.GetFactory("System.Data.SqlClient"), connStr) {}
+		public Database(string connStr) : this(connStr, DbProviderFactories.GetFactory("System.Data.SqlClient")) {}
 
-		public Database(DbProviderFactory factory, string connStr)
+		public Database(string connStr, DbProviderFactory factory)
 		{
 			_factory = factory;
 			_connStr = connStr;
