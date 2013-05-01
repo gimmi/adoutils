779e48d0e9734c9b9ebdde9568dd87a4577f9b52
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-05-01 18:38:30 +0200

    cosmetics

--- src/ADOUtils/IDatabase.cs
+++ src/ADOUtils/IDatabase.cs
@@ -11,6 +11,14 @@ namespace ADOUtils
 		T Scalar<T>(string sql);
 		T Scalar<T>(string sql, object parameters);
 		T Scalar<T>(string sql, IDictionary<string, object> parameters);
+		int Exec(string sql);
+		int Exec(string sql, object parameters);
+		int Exec(string sql, IDictionary<string, object> parameters);
+		ICommand CreateCommand();
+		IEnumerable<IDataRecord> Query(string sql);
+		IEnumerable<IDataRecord> Query(string sql, object parameters);
+		IEnumerable<IDataRecord> Query(string sql, IDictionary<string, object> parameters);
+
 		[Obsolete("Use Query(...).AsDisconnected().ToList() instead.")]
 		IEnumerable<IDataRecord> Read(string sql);
 		[Obsolete("Use Query(...).AsDisconnected().ToList() instead.")]
@@ -23,12 +31,5 @@ namespace ADOUtils
 		IEnumerable<IDataRecord> Yield(string sql, object parameters);
 		[Obsolete("Use Query(...) method instead.")]
 		IEnumerable<IDataRecord> Yield(string sql, IDictionary<string, object> parameters);
-		int Exec(string sql);
-		int Exec(string sql, object parameters);
-		int Exec(string sql, IDictionary<string, object> parameters);
-		ICommand CreateCommand();
-		IEnumerable<IDataRecord> Query(string sql, object parameters);
-		IEnumerable<IDataRecord> Query(string sql);
-		IEnumerable<IDataRecord> Query(string sql, IDictionary<string, object> parameters);
 	}
 }
